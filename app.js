f2a64dde21d8f84a3aaec12e9251e1fb04c1943b
shristi11 <shristi.ui@gmail.com>
2016-10-03 02:36:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 
+(function() {
+   angular.module("assignment1", [])
+   .controller("lunchCheck", ['$scope', function($scope) {
+   	  $scope.mylunch = ""; 
+       $scope.checking = function() {
+       	var lunches = $scope.mylunch.split(",");
+       	console.log(lunches.length);
+       	if(lunches.length == 1) {
+       		$scope.message = "Please enter data first";
+       
+       	} else if(lunches.length > 1 && lunches.length < 4) {
+       		$scope.message = "Enjoy!";
+       	} else if(lunches.length > 3) {
+       		$scope.message =  "Too much!";
+       	}
+       }
+   }]);
+
+  
+})();
